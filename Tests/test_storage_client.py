cf1ee02e9c7061023c03e7ca3160a1dd4133196b
Lars Dam <larsphdam@gmail.com>
2024-03-22 10:13:30 +0100

    create test files

---
+++ Tests/test_storage_client.py
@@ -0,0 +1,32 @@
+import unittest
+import os
+import sys
+
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(project_root)
+
+from IntegrationServer.StorageApi.storage_client import StorageClient   
+
+class TestStorageService(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(self):
+        pass
+
+    @classmethod
+    def tearDownClass(self):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_test(self):
+        self.assertEqual(1, 1, "its working")
+
+
+
+if __name__ == "__main__":
+    unittest.main()
