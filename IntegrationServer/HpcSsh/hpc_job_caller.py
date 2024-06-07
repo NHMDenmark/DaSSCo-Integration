1cf1a8c4e6a99c8b4cf55d47c9503258b33f63c2
Lars Dam <larsphdam@gmail.com>
2024-06-07 09:55:42 +0200

    Update hpc job caller with logging, pause loop and ssh connection error handling

--- IntegrationServer/HpcSsh/hpc_job_caller.py
+++ IntegrationServer/HpcSsh/hpc_job_caller.py
@@ -5,44 +5,61 @@ project_root = os.path.abspath(os.path.join(script_dir, '..'))
 sys.path.append(project_root)
 
 from Connections import connections
-from MongoDB import mongo_connection
+from MongoDB import track_repository
 from Enums import status_enum, validate_enum
 import utility
 import time
+from HealthUtility import health_caller
+from InformationModule.log_class import LogClass
 
 """
 Responsible for checking availability on slurm. Will have to wait for later to be made. For now assuming there always is capacity. 
 """
 # TODO Check that HPC is available. Get some status from mongo db. Other service responsible for updating that. 
-class HPCJobCaller:
+class HPCJobCaller(LogClass):
 
     def __init__(self):
 
+        # setting up logging
+        super().__init__(filename = f"{os.path.basename(os.path.abspath(__file__))}.log", name = os.path.relpath(os.path.abspath(__file__), start=project_root))
+        # service name for logging/info purposes
+        self.service_name = "HPC job caller"
+
         self.ssh_config_path = f"{project_root}/ConfigFiles/ucloud_connection_config.json"
         self.job_detail_path = f"{project_root}/ConfigFiles/job_detail_config.json"
-
-        self.run = True
-        self.count = 2
-
-        self.cons = connections.Connections()
+        self.run_config_path = f"{project_root}/ConfigFiles/run_config.json"
+        self.mongo_track = track_repository.TrackRepository()
         self.util = utility.Utility()
-        self.mongo_track = mongo_connection.MongoConnection("track")
+        self.health_caller = health_caller.HealthCaller()
+        self.status_enum = status_enum.StatusEnum
+        self.cons = connections.Connections()
 
-        self.cons.create_ssh_connection(self.ssh_config_path)
-        self.con = self.cons.get_connection()
+        # set the config file value to RUNNING, mostly for ease of testing
+        self.util.update_json(self.run_config_path, self.service_name, self.status_enum.RUNNING.value)
 
+        self.con = self.create_ssh_connection()
+
+        self.run = self.util.get_value(self.run_config_path, self.service_name)        
         self.loop()
     
-    def loop(self):
+    def create_ssh_connection(self):
+        self.cons.create_ssh_connection(self.ssh_config_path)
+        # handle when connection wasnt established - calls health service and sets run config to STOPPED
+        if self.cons.exc is not None:
+            entry = self.log_exc(self.cons.msg, self.cons.exc, self.status_enum.ERROR.value)
+            self.health_caller.warning(self.service_name, entry)
+            self.util.update_json(self.run_config_path, self.service_name, self.status_enum.STOPPED.value)
         
-        while self.run:
+        return self.cons.get_connection()
+
+    def loop(self):
+
+        while self.run == status_enum.StatusEnum.RUNNING.value:
             
-            asset = None
             asset = self.mongo_track.get_entry_from_multiple_key_pairs([{"hpc_ready": validate_enum.ValidateEnum.YES.value, 
                                                                          "jobs_status": status_enum.StatusEnum.WAITING.value}])
             
             if asset is None:
-                
                 time.sleep(1)        
             else:    
                 guid, jobs = self.get_guid_and_jobs(asset)
@@ -62,22 +79,34 @@ class HPCJobCaller:
                     time.sleep(1)
 
             # checks if service should keep running - configurable in ConfigFiles/run_config.json
-            run_config_path = f"{project_root}/ConfigFiles/run_config.json"
-            
-            all_run = self.util.get_value(run_config_path, "all_run")
-            service_run = self.util.get_value(run_config_path, "hpc_job_caller_run")
+            all_run = self.util.get_value(self.run_config_path, "all_run")
+            service_run = self.util.get_value(self.run_config_path, self.service_name)
 
-            if all_run == "False" or service_run == "False":
-                self.run = False
-                self.mongo_track.close_mdb()
-                self.cons.close_connection()
+            # Pause loop
+            counter = 0
+            while service_run == self.status_enum.PAUSED.value:
+                sleep = 10
+                counter += 1
+                time.sleep(sleep)
+                wait_time = sleep * counter
+                entry = self.log_msg(f"{self.service_name} has been in pause mode for ~{wait_time} seconds")
+                self.health_caller.warning(self.service_name, entry)
+                service_run = self.util.get_value(self.run_config_path, self.service_name)
+                
+                all_run = self.util.get_value(self.run_config_path, "all_run")
+                if all_run == self.status_enum.STOPPED.value:
+                    service_run = self.status_enum.STOPPED.value
+                
+                if service_run != self.status_enum.PAUSED.value:
+                    entry = self.log_msg(f"{self.service_name} has changed run status from {self.status_enum.PAUSED.value} to {service_run}")                   
+                    self.health_caller.warning(self.service_name, entry)
 
-            self.count -= 1
+            if all_run == self.status_enum.STOPPED.value or service_run == self.status_enum.STOPPED.value:
+                self.run = self.status_enum.STOPPED.value
 
-            if self.count == 0:
-                self.run = False
-                self.mongo_track.close_mdb()
-                self.cons.close_connection()
+        # outside main while loop        
+        self.mongo_track.close_connection()
+        self.cons.close_connection()
 
 
     def get_guid_and_jobs(self, asset):
