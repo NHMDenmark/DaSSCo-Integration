25521d85b44732ff9d787fc55c969a410399b84a
Lars Dam <larsphdam@gmail.com>
2024-06-10 11:25:46 +0200

    add logging and pause functionality to ndrive new files and process new files from ndrive

--- IntegrationServer/Ndrive/ndrive_new_files.py
+++ IntegrationServer/Ndrive/ndrive_new_files.py
@@ -7,50 +7,72 @@ sys.path.append(project_root)
 import shutil
 import time
 import utility
+from InformationModule.log_class import LogClass
+from HealthUtility import health_caller
+from Enums import status_enum
 
 """
 Class responsible for initiating the process of importing new files from the ndrive. 
 Runs a loop that checks the ndrive for previously not imported files.
+Logs warnings and errors from this process, and directs them to the health service.
 """
-
-
-class NdriveNewFilesFinder:
+class NdriveNewFilesFinder(LogClass):
 
     def __init__(self):
 
-        self.util = utility.Utility()
+        # setting up logging
+        super().__init__(filename = f"{os.path.basename(os.path.abspath(__file__))}.log", name = os.path.relpath(os.path.abspath(__file__), start=project_root))
+        # service name for logging/info purposes
+        self.service_name = "New files finder (Ndrive)"
 
-        self.new_files_path = f"{project_root}/Files/NewFiles"
+        self.util = utility.Utility()
+        self.run_config_path = f"{project_root}/ConfigFiles/run_config.json"
         self.workstations_config_path = f"{project_root}/ConfigFiles/workstations_config.json"
         self.ndrive_import_path = self.util.get_value(f"{project_root}/ConfigFiles/ndrive_path_config.json", "ndrive_path")
-        
-        self.run = True
-        self.count = 2
+        self.new_files_path = f"{project_root}/Files/NewFiles"
+        self.health_caller = health_caller.HealthCaller()
+        self.status_enum = status_enum.StatusEnum
 
+        # set the config file value to RUNNING, mostly for ease of testing
+        self.util.update_json(self.run_config_path, self.service_name, self.status_enum.RUNNING.value)
+        
+        self.run = self.util.get_value(self.run_config_path, self.service_name)
         self.loop()
 
     def loop(self):
 
-        while self.run:
+        while self.run == self.status_enum.RUNNING.value:
 
             self.copy_from_ndrive_and_update_ndrive_dirs(self.ndrive_import_path, self.new_files_path)
 
             time.sleep(1)
 
-            # checks if service should keep running - configurable in ConfigFiles/run_config.json
-            run_config_path = f"{project_root}/ConfigFiles/run_config.json"
-            
-            all_run = self.util.get_value(run_config_path, "all_run")
-            service_run = self.util.get_value(run_config_path, "ndrive_new_files_run")
+           # checks if service should keep running - configurable in ConfigFiles/run_config.json            
+            all_run = self.util.get_value(self.run_config_path, "all_run")
+            service_run = self.util.get_value(self.run_config_path, self.service_name)
 
-            if all_run == "False" or service_run == "False":
-                self.run = False
+            # Pause loop
+            counter = 0
+            while service_run == self.status_enum.PAUSED.value:
+                sleep = 10
+                counter += 1
+                time.sleep(sleep)
+                wait_time = sleep * counter
+                entry = self.log_msg(f"{self.service_name} has been in pause mode for ~{wait_time} seconds")
+                self.health_caller.warning(self.service_name, entry)
+                service_run = self.util.get_value(self.run_config_path, self.service_name)
                 
+                all_run = self.util.get_value(self.run_config_path, "all_run")
+                if all_run == self.status_enum.STOPPED.value:
+                    service_run = self.status_enum.STOPPED.value
+                
+                if service_run != self.status_enum.PAUSED.value:
+                    entry = self.log_msg(f"{self.service_name} has changed run status from {self.status_enum.PAUSED.value} to {service_run}")                   
+                    self.health_caller.warning(self.service_name, entry)
 
-            self.count -= 1
-
-            if self.count == 0:
-                self.run = False
+            if all_run == self.status_enum.STOPPED.value or service_run == self.status_enum.STOPPED.value:
+                self.run = self.status_enum.STOPPED.value
+                
 
     def copy_from_ndrive_and_update_ndrive_dirs(self, ndrive_path, local_destination):
 
@@ -110,6 +132,10 @@ class NdriveNewFilesFinder:
         # Rename the directory
         os.rename(old_path, new_path)
 
+    """
+    Find a unimported batch directory directory path based on the keys found in the workstation config file.
+    Will only return one batch directory path even if multiple exists. 
+    """
     def get_batch_directory_path(self, remote_folder):
         # Read workstation configuration data from JSON file
         workstations_config_data = self.util.read_json(self.workstations_config_path)
--- IntegrationServer/Ndrive/process_files_from_ndrive.py
+++ IntegrationServer/Ndrive/process_files_from_ndrive.py
@@ -6,50 +6,77 @@ sys.path.append(project_root)
 
 from JobList import job_driver
 import time
+import utility
+from InformationModule.log_class import LogClass
+from HealthUtility import health_caller
+from Enums import status_enum
 
 """
 Class responsible for initiating the processing of new files from the ndrive. Specifically this means we have
-both a json and an img file of some kind. Calls the JobDriver class for actual processing. 
+both a json and an img file of some kind. Calls the JobDriver class for actual processing.
+Logs warnings and errors from this process, and directs them to the health service.
 """
 
 # TODO needs to decide if a timer or trigger system is needed here.
 
 
-class ProcessNewFiles:
+class ProcessNewFiles(LogClass):
 
     def __init__(self):
+        
+        # setting up logging
+        super().__init__(filename = f"{os.path.basename(os.path.abspath(__file__))}.log", name = os.path.relpath(os.path.abspath(__file__), start=project_root))
+        # service name for logging/info purposes
+        self.service_name = "Process new files (Ndrive)"
 
         self.jobby = job_driver.JobDriver()
-
+        self.run_config_path = f"{project_root}/ConfigFiles/run_config.json"
         self.new_files_path = f"{project_root}/Files/NewFiles"
         self.updated_files_path = f"{project_root}/Files/UpdatedFiles"
+        self.health_caller = health_caller.HealthCaller()
+        self.util = utility.Utility()
+        self.status_enum = status_enum.StatusEnum
+
+        # set the config file value to RUNNING, mostly for ease of testing
+        self.util.update_json(self.run_config_path, self.service_name, self.status_enum.RUNNING.value)
         
-        self.run = True
-        self.count = 3
+        self.run = self.util.get_value(self.run_config_path, self.service_name)
 
         self.loop()
 
     def loop(self):
 
-        while self.run:
+        while self.run == self.status_enum.RUNNING.value:
 
             self.jobby.process_new_directories_from_ndrive()
 
             time.sleep(2)
 
-            # checks if service should keep running - configurable in ConfigFiles/run_config.json
-            run_config_path = f"{project_root}/ConfigFiles/run_config.json"
-            
-            all_run = self.util.get_value(run_config_path, "all_run")
-            service_run = self.util.get_value(run_config_path, "process_files_from_ndrive_run")
+            # checks if service should keep running - configurable in ConfigFiles/run_config.json            
+            all_run = self.util.get_value(self.run_config_path, "all_run")
+            service_run = self.util.get_value(self.run_config_path, self.service_name)
 
-            if all_run == "False" or service_run == "False":
-                self.run = False
+            # Pause loop
+            counter = 0
+            while service_run == self.status_enum.PAUSED.value:
+                sleep = 10
+                counter += 1
+                time.sleep(sleep)
+                wait_time = sleep * counter
+                entry = self.log_msg(f"{self.service_name} has been in pause mode for ~{wait_time} seconds")
+                self.health_caller.warning(self.service_name, entry)
+                service_run = self.util.get_value(self.run_config_path, self.service_name)
+                
+                all_run = self.util.get_value(self.run_config_path, "all_run")
+                if all_run == self.status_enum.STOPPED.value:
+                    service_run = self.status_enum.STOPPED.value
+                
+                if service_run != self.status_enum.PAUSED.value:
+                    entry = self.log_msg(f"{self.service_name} has changed run status from {self.status_enum.PAUSED.value} to {service_run}")                   
+                    self.health_caller.warning(self.service_name, entry)
 
-            self.count -= 1
-
-            if self.count == 0:
-                self.run = False
+            if all_run == self.status_enum.STOPPED.value or service_run == self.status_enum.STOPPED.value:
+                self.run = self.status_enum.STOPPED.value
 
 
 if __name__ == '__main__':
