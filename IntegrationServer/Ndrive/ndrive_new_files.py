739c8c7a1beea4215934b86c5034aac5a7c27f87
Lars Dam <larsphdam@gmail.com>
2024-06-21 15:20:27 +0200

    update ndrive new files with run utility and mongo db service repository

--- IntegrationServer/Ndrive/ndrive_new_files.py
+++ IntegrationServer/Ndrive/ndrive_new_files.py
@@ -7,8 +7,8 @@ sys.path.append(project_root)
 import shutil
 import time
 import utility
-from InformationModule.log_class import LogClass
-from HealthUtility import health_caller
+from MongoDB import service_repository
+from HealthUtility import health_caller, run_utility
 from Enums import status_enum
 
 """
@@ -16,27 +16,33 @@ Class responsible for initiating the process of importing new files from the ndr
 Runs a loop that checks the ndrive for previously not imported files.
 Logs warnings and errors from this process, and directs them to the health service.
 """
-class NdriveNewFilesFinder(LogClass):
+class NdriveNewFilesFinder():
 
     def __init__(self):
 
-        # setting up logging
-        super().__init__(filename = f"{os.path.basename(os.path.abspath(__file__))}.log", name = os.path.relpath(os.path.abspath(__file__), start=project_root))
+        self.log_filename = f"{os.path.basename(os.path.abspath(__file__))}.log"
+        self.logger_name = os.path.relpath(os.path.abspath(__file__), start=project_root)
+        
         # service name for logging/info purposes
         self.service_name = "New files finder (Ndrive)"
 
         self.util = utility.Utility()
-        self.run_config_path = f"{project_root}/ConfigFiles/run_config.json"
+        
         self.workstations_config_path = f"{project_root}/ConfigFiles/workstations_config.json"
         self.ndrive_import_path = self.util.get_value(f"{project_root}/ConfigFiles/ndrive_path_config.json", "ndrive_path")
         self.new_files_path = f"{project_root}/Files/NewFiles"
+        self.service_mongo = service_repository.ServiceRepository()
         self.health_caller = health_caller.HealthCaller()
         self.status_enum = status_enum.StatusEnum
+        self.run_util = run_utility.RunUtility(self.service_name, self.log_filename, self.logger_name)
 
-        # set the config file value to RUNNING, mostly for ease of testing
-        self.util.update_json(self.run_config_path, self.service_name, self.status_enum.RUNNING.value)
+       # set the service db value to RUNNING, mostly for ease of testing
+        self.service_mongo.update_entry(self.service_name, "run_status", self.status_enum.RUNNING.value)
         
-        self.run = self.util.get_value(self.run_config_path, self.service_name)
+        entry = self.run_util.log_msg(f"{self.service_name} status changed at initialisation to {self.status_enum.RUNNING.value}")
+        self.health_caller.run_status_change(self.service_name, self.status_enum.RUNNING.value, entry)
+
+        self.run = self.run_util.get_service_run_status()
         self.loop()
 
     def loop(self):
@@ -47,31 +53,15 @@ class NdriveNewFilesFinder(LogClass):
 
             time.sleep(1)
 
-           # checks if service should keep running - configurable in ConfigFiles/run_config.json            
-            all_run = self.util.get_value(self.run_config_path, "all_run")
-            service_run = self.util.get_value(self.run_config_path, self.service_name)
+           # checks if service should keep running           
+            self.run = self.run_util.check_run_changes()
 
             # Pause loop
-            counter = 0
-            while service_run == self.status_enum.PAUSED.value:
-                sleep = 10
-                counter += 1
-                time.sleep(sleep)
-                wait_time = sleep * counter
-                entry = self.log_msg(f"{self.service_name} has been in pause mode for ~{wait_time} seconds")
-                self.health_caller.warning(self.service_name, entry)
-                service_run = self.util.get_value(self.run_config_path, self.service_name)
-                
-                all_run = self.util.get_value(self.run_config_path, "all_run")
-                if all_run == self.status_enum.STOPPED.value:
-                    service_run = self.status_enum.STOPPED.value
-                
-                if service_run != self.status_enum.PAUSED.value:
-                    entry = self.log_msg(f"{self.service_name} has changed run status from {self.status_enum.PAUSED.value} to {service_run}")                   
-                    self.health_caller.warning(self.service_name, entry)
-
-            if all_run == self.status_enum.STOPPED.value or service_run == self.status_enum.STOPPED.value:
-                self.run = self.status_enum.STOPPED.value
+            if self.run == self.status_enum.PAUSED.value:
+                self.run = self.run_util.pause_loop()
+        
+        # out of main loop
+        self.service_mongo.close_connection()
                 
 
     def copy_from_ndrive_and_update_ndrive_dirs(self, ndrive_path, local_destination):
