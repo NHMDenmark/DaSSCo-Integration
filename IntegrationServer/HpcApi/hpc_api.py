d03105f996015f4222b11ef89dbc015b9d7a4df7
Lars Dam <larsphdam@gmail.com>
2024-08-29 15:38:09 +0200

    fix bug hpc api asset clean up endpoint

--- IntegrationServer/HpcApi/hpc_api.py
+++ IntegrationServer/HpcApi/hpc_api.py
@@ -121,7 +121,7 @@ async def file_info(file_info: file_info_model):
     if added is False:
         return JSONResponse(content={"error": "asset not found for file info"}, status_code=422)
 
-@app.post("/api/v1/assset_clean_up")
+@app.post("/api/v1/asset_clean_up")
 async def file_uploaded(asset_guid: str):
     cleaned = service.clean_up(asset_guid)
 
