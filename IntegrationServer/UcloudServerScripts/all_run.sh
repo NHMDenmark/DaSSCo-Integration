f022a74d745ac94d7fc246b8436e03dc4760cece
Lars Dam <larsphdam@gmail.com>
2024-08-29 13:27:43 +0200

    update all_run script to include open_share, close_share and hpc_clean_up

--- IntegrationServer/UcloudServerScripts/all_run.sh
+++ IntegrationServer/UcloudServerScripts/all_run.sh
@@ -9,9 +9,12 @@ nohup python "$root/HpcSsh/hpc_asset_creator.py" > "$root/HpcSsh/asset_creator.o
 nohup python "$root/HpcSsh/hpc_job_caller.py" > "$root/HpcSsh/job_caller.out" 2>&1 &
 nohup python "$root/HpcSsh/hpc_open_share.py" > "$root/HpcSsh/open_share.out" 2>&1 &
 nohup python "$root/HpcSsh/hpc_uploader.py" > "$root/HpcSsh/uploader.out" 2>&1 &
+nohup python "$root/HpcSsh/hpc_clean_up.py" > "$root/HpcSsh/hpc_clean_up.out" 2>&1 &
 
 nohup python "$root/StorageUpdater/asset_creator.py" > "$root/StorageUpdater/asset_creator.out" 2>&1 &
 nohup python "$root/StorageUpdater/file_uploader.py" > "$root/StorageUpdater/file_uploader.out" 2>&1 &
 nohup python "$root/StorageUpdater/sync_erda.py" > "$root/StorageUpdater/sync_erda.out" 2>&1 &
 nohup python "$root/StorageUpdater/update_metadata.py" > "$root/StorageUpdater/update_metadata.out" 2>&1 &
 nohup python "$root/StorageUpdater/validate_erda_sync.py" > "$root/StorageUpdater/validate_erda.out" 2>&1 &
+nohup python "$root/StorageUpdater/open_share.py" > "$root/StorageUpdater/open_share.out" 2>&1 &
+nohup python "$root/StorageUpdater/close_share.py" > "$root/StorageUpdater/close_share.out" 2>&1 &
