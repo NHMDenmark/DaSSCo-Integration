c65744225bb767b4c3743f5844013a995a8d0b65
Baeist <larsphdam@gmail.com>
2024-10-22 12:26:00 +0200

    add/update unit tests for hpc api

--- Tests/test_hpc_api.py
+++ Tests/test_hpc_api.py
@@ -46,17 +46,21 @@ class TestHPCApi(unittest.TestCase):
         response = self.client.get("/dev/api/v1/metadata_asset", params = {"asset_guid": test_guid})
         self.assertEqual(response.status_code, 422, f"Failed with a status {response.status_code} instead of 422")
     
+    """
+    deprecated not in use
     def test_receive_metadata(self):
         # can come later
         pass
-
+    """
+    
     def test_get_httplink(self):
         test_guid = "test_0001"
         response = self.client.get("/dev/api/v1/httplink", params = {"asset_guid": test_guid})
         self.assertEqual(response.status_code, 200, f"Failed with a status {response.status_code}")
         data = response.json()
-        link = "test/link/"
-        self.assertEqual(data["link"], link, f"Failed finding link as test/link/, found: {response.content}")
+        links = ["test/link/", ""]
+        self.assertIn(data["link"], links, f"Failed finding link as test/link/ or "", found: {response.content}")
+        
 
         test_guid = "bogus"
         response = self.client.get("/dev/api/v1/httplink", params = {"asset_guid": test_guid})
@@ -103,6 +107,18 @@ class TestHPCApi(unittest.TestCase):
         response = self.client.post("/dev/api/v1/start_job", data= model_json)
         self.assertEqual(response.status_code, 422, f"Failed with a status {response.status_code}")
 
+    def test_derivative_uploaded(self):
+        pass
+    
+    def test_asset_clean_up(self):
+        test_guid = "bogus"
+        response = self.client.post("/dev/api/v1/asset_clean_up", params = {"asset_guid": test_guid})
+        self.assertEqual(response.status_code, 422, f"Expected status 422, got {response.status_code}")
+
+        test_guid = "test_0001"
+        response = self.client.post("/dev/api/v1/asset_clean_up", params = {"asset_guid": test_guid})
+        self.assertEqual(response.status_code, 200, f"Expected status 200, got {response.status_code}")
+
     def test_update_asset(self):
         test_model = {
             "guid": "test_0001",
@@ -121,6 +137,31 @@ class TestHPCApi(unittest.TestCase):
         model_json = json.dumps(test_model)
         response = self.client.post("/dev/api/v1/update_asset", data= model_json)
         self.assertEqual(response.status_code, 422, f"Failed with a status {response.status_code}")
+
+    def test_derivative_file_info(self):
+        test_model = {
+            "guid": "test_0001",
+            "name": "test_0001.jpeg",
+            "type": "jpeg",
+            "check_sum": 12345,
+            "file_size": 200
+        }
+        model_json = json.dumps(test_model)
+
+        response = self.client.post("/dev/api/v1/derivative_file_info", data = model_json)
+        self.assertEqual(response.status_code, 200, f"Expected status 200 got {response.status_code}")
+
+        file_list = self.track_repo.get_value_for_key("test_0001", "file_list")
+        check_sum = file_list[1]["check_sum"]
+        self.assertEqual(check_sum, 12345, f"Expected check sum 12345 got {check_sum}")
+
+        asset_size = self.track_repo.get_value_for_key("test_0001", "asset_size")
+        self.assertEqual(asset_size, -191, f"Exepcted asset size to be -191 (9 + 200 - 400) got {asset_size}")
+
+        test_model["guid"] = "bogus"
+        model_json = json.dumps(test_model)
+        response = self.client.post("/dev/api/v1/derivative_file_info", data = model_json)
+        self.assertEqual(response.status_code, 422, f"Expected status 422 got {response.status_code}")
 
     def test_barcode(self):
         test_model = {
